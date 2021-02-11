50a9b358baf863971a784ce1283a14c568551236
choon <chachooon@gmail.com>
2021-02-11 17:33:06 +0900

    feat: :package: add @apollo/client & apollo-server

--- README.md
+++ README.md
@@ -23,3 +23,14 @@ Make by Cha and Kan
 - [ ] Feed CRUD
 - [ ] Subscribe/Unsubscribe User
 - [ ] Like/Unlike Feed
+
+## Quick Start
+
+```bash
+# 1. install package
+npm i && cd ios && pod install && cd ..
+# 2. apollo-server 시작하기
+node server.js
+# 3. ios app 시작하기 (새 터미널에서 실행)
+npm run ios
+```
--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,45 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
 import styled from '@emotion/native';
+import Home from './containers/Home';
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  ApolloLink,
+  HttpLink,
+} from '@apollo/client';
+import { onError } from '@apollo/client/link/error';
 
 const App = () => {
+  const link = onError(({ graphQLErrors, networkError }) => {
+    if (graphQLErrors)
+      graphQLErrors.map(({ message, locations, path }) =>
+        console.log(
+          `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+        ),
+      );
+
+    if (networkError) console.log(`[Network error]: ${networkError}`);
+  });
+
+  const client = new ApolloClient({
+    uri: 'http://localhost:4000/',
+    cache: new InMemoryCache(),
+    link: ApolloLink.from([
+      link,
+      new HttpLink({ uri: 'http://localhost:4000/' }),
+    ]),
+  });
   return (
-    <>
+    <ApolloProvider client={client}>
       <StatusBar barStyle="dark-content" />
       <Container>
         <Logo source={require('~/assets/images/logo.jpg')} />
         <LogoText source={require('~/assets/images/logo_text.jpg')} />
+        <Home />
       </Container>
-    </>
+    </ApolloProvider>
   );
 };
 
